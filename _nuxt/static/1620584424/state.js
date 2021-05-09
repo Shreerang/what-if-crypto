5be5114f8f37dc4ca7aed551346ef12831b36497
Shreerang Patwardhan <spatwardhan@hq.bill.com>
2021-05-09 11:20:29 -0700

    cc565c1

---
+++ _nuxt/static/1620584424/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fwhat-if-crypto\u002F_nuxt\u002Fstatic\u002F1620584424",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fwhat-if-crypto\u002F",assetsPath:"\u002Fwhat-if-crypto\u002F_nuxt\u002F",cdnURL:a}}}}(null));
